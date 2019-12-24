9753aab1d8f601679954ccc726383526fd4a7a66
Eliza Ahmed <elizarafee@gmail.com>
2019-12-24 20:57:00 +0600

    users table migration updated

---
+++ database/migrations/2019_12_24_141924_update_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('name', 'first_name');
+            $table->string('last_name')->after('name');
+            $table->dropUnique('users_email_unique');
+            $table->timestamp('last_logged_in_at', 0)->after('remember_token')->nullable();
+            $table->integer('created_by')->after('created_at')->nullable();
+            $table->integer('updated_by')->after('updated_at')->nulllable();
+            $table->timestamp('deleted_at', 0)->nullable();
+            $table->integer('deleted_by')->nulllable();
+        });        
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
