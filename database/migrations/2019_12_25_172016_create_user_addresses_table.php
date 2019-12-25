34df98c3c62e3c11aac0e53f7351dd2d258576f7
Eliza Ahmed <elizarafee@gmail.com>
2019-12-25 17:32:46 +0000

    user addresses table added

---
+++ database/migrations/2019_12_25_172016_create_user_addresses_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('type');
+            $table->string('address_line_1');
+            $table->string('address_line_2')->nullable();
+            $table->string('address_line_3')->nullable();
+            $table->string('city');
+            $table->string('postcode');
+            $table->string('country');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->integer('updated_by')->nullable();
+            $table->integer('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //Schema::dropIfExists('user_addresses');
+    }
+}
+
